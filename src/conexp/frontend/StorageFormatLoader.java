1312bd35363d7797155adfcc98a94733e9a8fa01
s_yevtushenko <>
2003-07-21 15:26:46 +0000

    done some code cleaning for StorageFormatLoader and tests

--- src/conexp/frontend/StorageFormatLoader.java
+++ src/conexp/frontend/StorageFormatLoader.java
@@ -1,18 +1,18 @@
 /*
  * Created on Mar 22, 2003
  *
- * the goal of this class is to load the different storage loader 
+ * the goal of this class is to load the different storage loader
  * */
 package conexp.frontend;
 
-import java.util.ArrayList;
+import conexp.frontend.util.StorageFormatManager;
+import conexp.frontend.util.StorageFormatRecord;
+import util.StringUtil;
+import util.collection.CollectionFactory;
+
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.StringTokenizer;
-
-
-import conexp.frontend.util.StorageFormatManager;
-import conexp.frontend.util.StorageFormatRecord;
 
 /**
  * @author Julien Tane
@@ -21,64 +21,65 @@ import conexp.frontend.util.StorageFormatRecord;
  */
 public class StorageFormatLoader {
 
-	public final static String  STORAGE_FORMAT_LIST_KEY = "conexp.storage.formats";
+    public final static String STORAGE_FORMAT_LIST_KEY = "conexp.storage.formats";
 
-	ResourceBundle resources = null;
-	
-	/**
-	 * 
-	 */
-	public StorageFormatLoader( ResourceBundle resources) {
-		super();
-		this.resources = resources;
-	}
+    ResourceBundle resources = null;
 
-	public String[]  loadStorageFormatList() {
-		String[] array =null;
-		List list = new ArrayList();
-		String keys= null;
-		try {
-		 keys=resources.getString(STORAGE_FORMAT_LIST_KEY);
-		} catch (RuntimeException e) {
-			
-			e.printStackTrace();
-		}
-		return getStringArray(keys, ","); 
-	}
-	
-	private String[] getStringArray(String keys,String sep){
-	String[] stringarray=null;
-	List list = new ArrayList();
-	String key = null;
-			for (StringTokenizer tok = new StringTokenizer(keys,sep); tok.hasMoreTokens();){
-				key = tok.nextToken();
-				key =key.trim();
-				list.add(key);
-			}
-		return (String[]) list.toArray(new String[0]);
-	}
+    public StorageFormatLoader(ResourceBundle resources) {
+        super();
+        this.resources = resources;
+    }
 
-	private Class loadClass(String classtoload)throws ClassNotFoundException{
-		return Class.forName(classtoload);
-	}
+    public String[] loadStorageFormatList() {
+        String keys = resources.getString(STORAGE_FORMAT_LIST_KEY);
+        return getStringArray(keys, ",");
+    }
 
-	public StorageFormatRecord createStorageFormatRecord(String description, String  extension, String docloader, String docwriter) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-			return new StorageFormatRecord(description, extension,(DocumentLoader) loadClass( docloader).newInstance(), (DocumentWriter)loadClass(docwriter).newInstance());
-	}
+    //todo: have a look at StringUtil or just create class PropertyUtils
 
-	public void loadStorageFormats(StorageFormatManager storageforamtmanager){
-		String[] storageformatlist = loadStorageFormatList();
-		if (storageformatlist == null) {System.out.println("null"); return;}
-		StorageFormatRecord sr= null;
-		String[] sd =null;// storage definition
-		for (int i =0; i< storageformatlist.length; i++){
-			try {
-				sd = getStringArray(resources.getString(storageformatlist[i]),",");
-				sr =createStorageFormatRecord(sd[0],sd[1],sd[2],sd[3]);
-				storageforamtmanager.registerStorageFormat(sr);
-			} catch (Exception e) {
-				System.err.println("The following StorageFormat could not be registered:"+ sd[0]+" due to Exception "+e);
-			}	
-		}	
-	}
+    private static String[] getStringArray(String keys, String sep) {
+        List list = CollectionFactory.createDefaultList();
+        keys = StringUtil.safeTrim(keys); //for safety
+        for (StringTokenizer tok = new StringTokenizer(keys, sep); tok.hasMoreTokens();) {
+            String key = tok.nextToken();
+            key = key.trim();
+            list.add(key);
+        }
+        return (String[]) list.toArray(new String[list.size()]);
+    }
+
+
+    public StorageFormatRecord createStorageFormatRecord(String description, String extension, String docloader, String docwriter) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        return new StorageFormatRecord(description, extension, (DocumentLoader) safeLoadClass(docloader), (DocumentWriter) safeLoadClass(docwriter));
+    }
+
+    private Object safeLoadClass(String docloader) throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        docloader = StringUtil.safeTrim(docloader);
+        if(StringUtil.isEmpty(docloader)){
+            return null;
+        }
+//todo: think about replacemant with    ReflectHelper.createClassByName()
+        return Class.forName(docloader).newInstance();
+    }
+
+    public void loadStorageFormats(StorageFormatManager storageforamtmanager) {
+        String[] storageformatlist = loadStorageFormatList();
+        //removed check for null,  because such situation can't appear : Sergey
+        //todo: Julian, please remove this two comments, when you read them
+        for (int i = 0; i < storageformatlist.length; i++) {
+            try {
+                String[] sd = getStringArray(resources.getString(storageformatlist[i]), ",");
+                StorageFormatRecord sr  = createStorageFormatRecord(sd[0], sd[1], sd[2], sd[3]);
+                storageforamtmanager.registerStorageFormat(sr);
+            } catch (Exception e) {
+                //may be here better replace it with some general log concole.
+
+                //removed reference to sd[0], because it can lead to null pointer exception, when resource
+                //bundle throws an exception . Sergey
+                //todo: Julian, please remove this three comments, when you read them
+
+                System.err.println("The following StorageFormat not be registered due to Exception " + e);
+            }
+        }
+    }
 }
--- src/conexp/frontend/tests/StorageFormatLoaderTests.java
+++ src/conexp/frontend/tests/StorageFormatLoaderTests.java
@@ -1,8 +1,3 @@
-/*
- * Created on Mar 22, 2003
- *
- * the goal of this class is to load the different storage loader 
- * */
 package conexp.frontend.tests;
 
 import java.io.IOException;
@@ -20,69 +15,29 @@ import conexp.frontend.StorageFormatLoader;
 import conexp.frontend.util.StorageFormatManager;
 import conexp.frontend.util.StorageFormatRecord;
 
-/**
- * @author Julien Tane
- *
- * Class to load automatically all the Storage Format into a list
- */
 public class StorageFormatLoaderTests extends TestCase{
 	
-	static ResourceBundle resources = null; 
-
-	String propertiesstring = "";
+	static ResourceBundle realresource = null;
 
 	static {
-		resources = ResourceLoader.getResourceBundle("conexp/frontend/resources/ContextDocManager");  //$NON-NLS-1$
+		realresource = ResourceLoader.getResourceBundle("conexp/frontend/resources/ContextDocManager");  //$NON-NLS-1$
 	}
-	
+
 
 	public void  testLoadStorageFormatList() {
-		setUp();
-		String[] loadstorageformatlist = getStorageFormatLoader().loadStorageFormatList();
-	}
-	
-	protected void setUp(){
-		ResourceBundle res=null;
-		try{// TODO the StringBufferInputStream is depreacated should be replaced by something else
-			 res =new PropertyResourceBundle(new StringBufferInputStream(propertiesstring));
-		}catch (IOException e){
-			System.out.println("the resource bundle for the test could not be loaded "+e);
-		}
-		resources = res;
-	}
-	
-	private StorageFormatLoader getStorageFormatLoader(){
-		return new StorageFormatLoader(getResourceBundle());
-	}
-	
-	private ResourceBundle getResourceBundle(){
-		return resources;
+		makeStorageFormatLoader(realresource).loadStorageFormatList();
 	}
 
+	private StorageFormatLoader makeStorageFormatLoader(ResourceBundle resourceBundle){
+		return new StorageFormatLoader(resourceBundle);
+	}
 
-	/**
-	 * returns a storageformatloader
-	 * @return StorageFormatManager
-	 */
-	private StorageFormatManager getStorageFormatManager() {
+	private StorageFormatManager makeStorageFormatManager() {
 		return new StorageFormatManager();
 	}
 	
-	public void testcreateStorageFormatRecord(String description, String  extension, String docloader, String docwriter) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-		setUp();		
-		StorageFormatRecord stfmtrecord =getStorageFormatLoader().createStorageFormatRecord(description, extension,docloader, docwriter);
-		assertNotNull(stfmtrecord);
-		assertEquals("createStorageFormat  does not correctly set the desctiption ", stfmtrecord.getExtension(), extension);
-		assertEquals("createStorageFormat  does not correctly set the extensions ", stfmtrecord.getExtension(), extension);
-		assertEquals("createStorageFormat  does not correctly set the DocumentLoader", stfmtrecord.getLoader(), docloader); 
-		assertEquals("createStorageFormat  does not correctly set theDocumentWriter ", stfmtrecord.getWriter(), docwriter);
-	}
-
 	public void testLoadStorageFormats(){
-		setUp();
-		StorageFormatManager storageformatmanager = getStorageFormatManager();
-		StorageFormatLoader formatLoader =  getStorageFormatLoader();
-		formatLoader.loadStorageFormats(storageformatmanager);		
+        makeStorageFormatLoader(realresource).loadStorageFormats(makeStorageFormatManager());
 	}
 
 }
