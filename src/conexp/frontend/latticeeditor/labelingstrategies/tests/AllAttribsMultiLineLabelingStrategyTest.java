37dacf78a0ecfdceabdcd6a875437ec4a48cf373
s_yevtushenko <>
2006-08-16 09:41:14 +0000

    Fixed build.xml to work with java 1.4 sources. Added missing files

---
+++ src/conexp/frontend/latticeeditor/labelingstrategies/tests/AllAttribsMultiLineLabelingStrategyTest.java
@@ -0,0 +1,45 @@
+package conexp.frontend.latticeeditor.labelingstrategies.tests;
+
+import conexp.core.Lattice;
+import conexp.core.tests.SetBuilder;
+import conexp.frontend.latticeeditor.LatticeDrawing;
+import conexp.frontend.latticeeditor.labelingstrategies.AllAttribsMultiLineLabelingStrategy;
+import conexp.frontend.latticeeditor.labelingstrategies.GenericLabelingStrategy;
+import conexp.frontend.latticeeditor.labelingstrategies.LabelingStrategiesKeys;
+import conexp.frontend.latticeeditor.queries.ConceptNodeQuery;
+import conexp.frontend.latticeeditor.queries.ConceptNodeQueryFactory;
+
+public class AllAttribsMultiLineLabelingStrategyTest extends GenericLabelingStrategyTest {
+    protected ConceptNodeQuery makeAcceptable() {
+        return ConceptNodeQueryFactory.makeWithOwnAttribs();
+    }
+
+    protected GenericLabelingStrategy makeStrategy() {
+        return new AllAttribsMultiLineLabelingStrategy();
+    }
+
+    public void testAdditionOfAttributesAndCleanup() {
+        Lattice lattice = SetBuilder.makeLatticeWithContext(new int[][]{{0, 1},
+                                                                        {1, 0}});
+        LatticeDrawing drawing = new LatticeDrawing();
+        drawing.setLattice(lattice);
+        assertTrue(drawing.setAttributeLabelingStrategyKey(LabelingStrategiesKeys.NO_ATTRIBS_LABELING_STRATEGY));
+        assertEquals(false, drawing.hasUpLabelsForConcepts());
+        GenericLabelingStrategy allAttribsLabelingStrategy = makeStrategy();
+
+        allAttribsLabelingStrategy.setContext(lattice.getContext());
+        allAttribsLabelingStrategy.init(drawing, makeDrawParams());
+        assertEquals(true, drawing.hasUpLabelsForConcepts());
+        allAttribsLabelingStrategy.shutdown(drawing);
+        assertEquals(false, drawing.hasUpLabelsForConcepts());
+
+    }
+
+    public void testGetLabelLocationAngleInRadians() {
+        AllAttribsMultiLineLabelingStrategy strategy =
+                ((AllAttribsMultiLineLabelingStrategy) labelStrategy);
+        assertEquals(1.5*Math.PI, strategy.getLabelLocationAngleInRadians(), 0.0001);
+
+    }
+
+}
---
+++ src/conexp/frontend/latticeeditor/labelingstrategies/tests/AllObjectsMultiLineLabelingStrategyTest.java
@@ -0,0 +1,44 @@
+package conexp.frontend.latticeeditor.labelingstrategies.tests;
+
+import conexp.core.Lattice;
+import conexp.core.tests.SetBuilder;
+import conexp.frontend.latticeeditor.LatticeDrawing;
+import conexp.frontend.latticeeditor.labelingstrategies.AllObjectsMultiLineLabelingStrategy;
+import conexp.frontend.latticeeditor.labelingstrategies.GenericLabelingStrategy;
+import conexp.frontend.latticeeditor.labelingstrategies.LabelingStrategiesKeys;
+import conexp.frontend.latticeeditor.queries.ConceptNodeQuery;
+import conexp.frontend.latticeeditor.queries.ConceptNodeQueryFactory;
+
+public class AllObjectsMultiLineLabelingStrategyTest extends GenericLabelingStrategyTest {
+    protected ConceptNodeQuery makeAcceptable() {
+        return ConceptNodeQueryFactory.makeWithOwnObjects();
+    }
+
+    protected GenericLabelingStrategy makeStrategy() {
+        return new AllObjectsMultiLineLabelingStrategy();
+    }
+
+    public void testAdditionOfAttributesAndCleanup() {
+        Lattice lattice = SetBuilder.makeLatticeWithContext(new int[][]{{0, 1},
+                                                                        {1, 0}});
+        LatticeDrawing drawing = new LatticeDrawing();
+        drawing.setLattice(lattice);
+        assertTrue(drawing.setObjectLabelingStrategyKey(LabelingStrategiesKeys.NO_OBJECTS_LABELS_STRATEGY));
+        assertEquals(false, drawing.hasDownLabelsForConcepts());
+        GenericLabelingStrategy alObjectsLabelingStrategy = makeStrategy();
+        alObjectsLabelingStrategy.setContext(lattice.getContext());
+        alObjectsLabelingStrategy.init(drawing, makeDrawParams());
+        assertEquals(true, drawing.hasDownLabelsForConcepts());
+        alObjectsLabelingStrategy.shutdown(drawing);
+        assertEquals(false, drawing.hasDownLabelsForConcepts());
+
+    }
+
+    public void testGetLabelLocationAngleInRadians() {
+        assertEquals(0.5*Math.PI,                                                     
+                getNativeStrategy().getLabelLocationAngleInRadians(),
+                0.0001);
+    }
+
+    private AllObjectsMultiLineLabelingStrategy getNativeStrategy() {return ((AllObjectsMultiLineLabelingStrategy)labelStrategy);}
+}
